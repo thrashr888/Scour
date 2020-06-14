15a638365d84816171407a2aae49b90dbee3e60f
Paul Thrasher <thrashr888@gmail.com>
2020-06-14 11:48:57 -0700

    Initial Commit

---
+++ Scour/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Scour
+//
+//  Created by Paul Thrasher on 6/14/20.
+//  Copyright © 2020 Paul Thrasher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
