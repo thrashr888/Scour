41cfa5c3f4795fb3d8f7c014dc21bda15a88fe89
Paul Thrasher <thrashr888@gmail.com>
2020-07-01 23:49:06 -0700

    add git library
    Signed-off-by: Paul Thrasher <thrashr888@gmail.com>

--- Scour/ContentView.swift
+++ Scour/ContentView.swift
@@ -7,8 +7,42 @@
 //
 
 import SwiftUI
+import SwiftGit2
+
+
+
+func loadRepo(path: String = "") {
+    let url: URL = URL(fileURLWithPath: path)
+    let result = Repository.at(url)
+    
+    switch result {
+    case let .success(repo):
+        let latestCommit = repo
+            .HEAD()
+            .flatMap {
+                repo.commit($0.oid)
+            }
+
+        switch latestCommit {
+        case let .success(commit):
+            print("Latest Commit: \(commit.message) by \(commit.author.name)")
+
+        case let .failure(error):
+            print("Could not get commit: \(error)")
+        }
+
+    case let .failure(error):
+        print("Could not open repository: \(error)")
+    }
+}
+
 
 struct ContentView: View {
+    init(){
+        loadRepo(path: "/Users/thrashr888/hashicorp/atlas")
+//        loadRepo(url: "https://github.com/SwiftGit2/SwiftGit2.git")
+    }
+    
     var body: some View {
         Text("Hello, World!")
             .frame(maxWidth: .infinity, maxHeight: .infinity)
